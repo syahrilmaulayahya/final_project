fa158428aa5b42a35cc082b312b7c5d0024a6161
syahrilmaulayahya <syahrilmaulayahya@gmail.com>
2021-09-17 14:52:38 +0700

    firstime commit GET users

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Users struct {
-	Id           int    `json:"id"`
+	UserId       int    `json:"userId"`
 	Username     string `json:"username"`
 	Picture_url  string `json:"picture_url"`
 	Phone_number int    `json:"phone_number"`
@@ -26,12 +27,24 @@ type BaseResponse struct {
 
 func main() {
 	e := echo.New()
-	e.GET("/v1/users", GetUserController)
+	ev1users := e.Group("v1/users")
+	ev1users.GET("", GetUserController)
+	ev1users.GET("/:userId", DetailUserController)
 	e.Start(":8000")
 }
 
 func GetUserController(c echo.Context) error {
 	user := Users{1, "puppy", "https://id.depositphotos.com/stock-photos/puppy.html", +62856414411299, "puppy@gmail.com", "pupy", "male", "2000-3-1"}
 	response := BaseResponse{http.StatusOK, "Success", user}
+	return c.JSON(http.StatusOK, response)
+}
+
+func DetailUserController(c echo.Context) error {
+	userId, err := strconv.Atoi(c.Param("userId"))
+	response := BaseResponse{http.StatusOK, "Success", Users{UserId: userId}}
+	failresponse := BaseResponse{http.StatusInternalServerError, "Gagal konversi userId", nil}
+	if err != nil {
+		return c.JSON(http.StatusOK, failresponse)
+	}
 	return c.JSON(http.StatusOK, response)
 }
