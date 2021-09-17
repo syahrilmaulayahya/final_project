644fdd3815bbca2d32890fa3417383428754fc83
syahrilmaulayahya <syahrilmaulayahya@gmail.com>
2021-09-17 11:41:30 +0700

    commiting GET users firstime

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Users struct {
+	Id           int    `json:"id"`
+	Username     string `json:"username"`
+	Picture_url  string `json:"picture_url"`
+	Phone_number int    `json:"phone_number"`
+	Email        string `json:"email"`
+	// Password     string `json:"password"`
+	Name   string `json:"name"`
+	Gender string `json:"gender"`
+	Dob    string `json:"dob"`
+}
+
+type BaseResponse struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
 
 func main() {
-	fmt.Println("Hello World Master")
+	e := echo.New()
+	e.GET("/v1/users", GetUserController)
+	e.Start(":8000")
+}
 
-	fmt.Println("Hello World A Pertama")
-
-	fmt.Println("Hello World B Pertama")
-
-	fmt.Println("Hello World A Bukan Stash")
-
-	fmt.Println("Hello World A Stash")
-	fmt.Println("Hello World A Stash")
-	fmt.Println("Hello World A Stash")
-	fmt.Println("Hello World A Stash")
-	fmt.Println("Hello World A Stash")
-
+func GetUserController(c echo.Context) error {
+	user := Users{1, "puppy", "https://id.depositphotos.com/stock-photos/puppy.html", +62856414411299, "puppy@gmail.com", "pupy", "male", "2000-3-1"}
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code:    http.StatusOK,
+		Message: "Success",
+		Data:    user,
+	})
 }
