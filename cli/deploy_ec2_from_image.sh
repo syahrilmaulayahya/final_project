4b73a084b5f171c602d2dc6da4a022503b878741
syahrilmaulayahya <syahrilmaulayahya@gmail.com>
2021-10-06 09:53:05 +0700

    add cd3

--- cli/deploy_ec2_from_image.sh
+++ cli/deploy_ec2_from_image.sh
@@ -1,2 +1,2 @@
-docker pull moryku/kampus_merdeka:1.0.0
-docker run -d -p 8080:8080 --name kampus_merdeka moryku/kampus_merdeka:1.0.0
+docker pull syahrilmaulayahya/kampus_merdeka:1.0.0
+docker run -d -p 8080:8080 --name kampus_merdeka syahrilmaulayahya/kampus_merdeka:1.0.0
