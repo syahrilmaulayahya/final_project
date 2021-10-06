f48d49f3156eda9cf05468a6dbb3332fde2462f4
syahrilmaulayahya <syahrilmaulayahya@gmail.com>
2021-10-06 09:20:57 +0700

    penambahan CD (continous deployment)

---
+++ cli/connec_ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo ssh -i "ecommerce.pem" ec2-user@ec2-18-216-253-33.us-east-2.compute.amazonaws.com
---
+++ cli/connect_rds.sh
@@ -0,0 +1,1 @@
+mysql -h db-ecommerce.cfwwyxjalxvb.us-east-2.rds.amazonaws.com -u root -p
